6193d0c804448a692a7531e3b2c4c296f3b6a0f0
Jrol123 <angap4@gmail.com>
2019-03-16 19:00:02 +1000

    HomeWork #0.5 Signed-off-by: Jrol123 <angap4@gmail.com>

---
+++ Artemii/HomeWork.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace procents_branchess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How much is the total purchase?");
+            double summ = double.Parse(Console.ReadLine());
+            Console.WriteLine(" ");
+            Console.WriteLine("What is the percentage discount?");
+            double discount = double.Parse(Console.ReadLine());
+            Console.WriteLine(" ");
+            Console.Write("Total puchase with discount:");
+            double total = summ-((summ / 100) * discount);
+            Console.WriteLine(total);
+            Console.ReadKey();
+        }
+    }
+}
