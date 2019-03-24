1a8e56f148bcd169b5bd010242e097e480cc3ca4
Artemii <47931983+Jrol123@users.noreply.github.com>
2019-03-24 11:57:05 +1000

    To Artemii!!

---
+++ FizzBuzz.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            while(a < 300)
+            {
+                ++a;
+                if (a % 3 == 0 && a % 5 == 0)
+                {
+                    Console.Write("FizzBuzz ");
+                }
+                else if (0 == a % 3 && 0 != a % 5)
+                {
+                    Console.Write("Fizz ");
+                }
+                else if (0 != a % 3 && 0 == a % 5)
+                {
+                    Console.Write("Buzz ");
+                }
+                else
+                {
+                    Console.Write(a + " " );
+                }
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
