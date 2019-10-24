4655295bdef42fd885a0ead38c268425621d9653
Artemii <47931983+Jrol123@users.noreply.github.com>
2019-10-24 17:32:34 +1000

    List

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("0 - exit");
+            Console.WriteLine("1 - give");
+            Console.WriteLine("2 - take");
+            Console.WriteLine("3 - add");
+            Console.WriteLine("4 - count");
+            Console.WriteLine("5 - statist of book");
+            Console.WriteLine("6 - add a whole new book");
+
+            List<Book> books = new List<Book>();
+            int a = int.Parse(Console.ReadLine());
+            while (a != 0)
+            {
+                switch (a)
+                {
+                    case 1:
+                        foreach()
+                        books[0].Give();
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+
+                        break;
+                    case 6:
+                        books.Add(new Book());
+                        break;
+
+
+                }
+            }
+        }
+    }
+}//Доделать методы + Доделать ввыдачу + доделать мелочь
